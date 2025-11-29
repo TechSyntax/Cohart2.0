eecee329a4c2da2201526a4a77f6e015e92f6901
Vishalpal <pal320542@gmail.com>
2025-11-29 16:07:07 +0530

    Hovering Text

--- functional.js
+++ functional.js
@@ -1,7 +1,23 @@
+let para = document.querySelector("p"); // or .para / #para
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let originalText = para.innerText;
 
-addEventListener("mousemove", function(e) {
-    console.log(e.clientX, e.clientY);
+para.addEventListener("mouseenter", () => {
+    let iteration = 0;
     
-    document.body.style.setProperty("--x", e.clientX + "px");
-    document.body.style.setProperty("--y", e.clientY + "px");
+    let interval = setInterval(() => {
+        para.innerText = originalText
+            .split("")
+            .map((char, index) => {
+                if (index < iteration) return originalText[index];
+                return characters[Math.floor(Math.random() * characters.length)];
+            })
+            .join("");
+
+        iteration += 0.99;
+
+        if (iteration >= originalText.length) {
+            clearInterval(interval);
+        }
+    }, 60);
 });
